289f9a4603153d93c2fd3fbda1c77df2bac88117
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2025-04-10 23:52:46 +0530

    Delete sb-ecom directory

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.sb_ecom.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.sb_ecom.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
