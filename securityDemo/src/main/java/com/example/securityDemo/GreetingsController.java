a914f606f38fd9208168194d47edfbd2df098cca
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2025-04-16 23:06:52 +0530

    Add files via upload

---
+++ securityDemo/src/main/java/com/example/securityDemo/GreetingsController.java
@@ -0,0 +1,28 @@
+package com.example.securityDemo;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+	@GetMapping("/home")
+	public String getMethodName() {
+		return "hello world";
+	}
+	
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String UsergetMethodName() {
+		return "hello user world";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String AdmingetMethodName() {
+		return "hello admin world";
+	}
+	
+}
